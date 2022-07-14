86d9572dc0bc57356b8f40602cca4a21582d7e19
AsyncTeamInc <asyncteaminc@gmail.com>
2022-07-14 23:53:45 +0530

    pdf project

---
+++ Storybook/products/react-invoice-generator-master/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import InvoicePage from './components/InvoicePage'
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="center fs-30">React Invoice Generator</h1>
+      <InvoicePage />
+    </div>
+  )
+}
+
+export default App
